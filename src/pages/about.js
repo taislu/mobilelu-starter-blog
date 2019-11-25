0408b1c019c7dad2968d12e7b4ab7ef18912fc54
Tai Lu <taislu@hotmail.com>
2019-11-25 09:28:13 -0800

    add about_us image

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from 'react'
 import NavLayout from '../components/NavLayout'
 import SEO from '../components/seo'
 import { PageHeader, Banner } from '../utils'
-import img from '../images/about_tlu.jpg'
+import img from '../images/about_us1.jpg'
 
 import OurMission from "../info/our_mission"
 
