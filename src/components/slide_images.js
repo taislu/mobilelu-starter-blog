cb142fd34cb391c872fccc2a7e14a7b6b6c3f604
Tai Lu <taislu@hotmail.com>
2019-12-17 10:52:22 -0800

    update

--- src/components/slide_images.js
+++ src/components/slide_images.js
@@ -1,4 +1,5 @@
 // 800 x 600 images
+/*
 import image_1 from "../slides/image_1.jpg"
 import image_2 from "../slides/image_2.jpg"
 import image_3 from "../slides/image_3.jpg"
@@ -8,8 +9,8 @@ import image_5 from "../slides/image_5.jpg"
 const images = [
   image_1, image_2, image_3, image_4, image_5
 ]
+*/
 
-/*
 const images = [
     "https://images.unsplash.com/photo-1559666126-84f389727b9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1356&q=80",
     "https://images.unsplash.com/photo-1557389352-e721da78ad9f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
@@ -17,6 +18,5 @@ const images = [
     "https://images.unsplash.com/photo-1550596334-7bb40a71b6bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
     "https://images.unsplash.com/photo-1550640964-4775934de4af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
   ];
-*/
 
 export default images
